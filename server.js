c50111ce60e04ba5982661ab0586bdd1043ae748
Darwin Donato <dvdonato-ctl@abs-cbn.com>
2020-03-24 19:27:00 +0800

    Update Server 4.0

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ var express = require('express');
 const path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/client-side'));
+app.use(express.static(__dirname + '/portfolio'));
 
-// all unmatched requests to this path, with no file extension, redirect to the dash page
-app.use('/', function ( req, res, next ) {
-  // uri has a forward slash followed any number of any characters except full stops (up until the end of the string)
-  if (/\/[^.]*$/.test(req.url)) {
-      res.sendfile(__dirname + req.params[0]);
-  } else {
-    res.sendFile(__dirname + 'client-side/index.html');
-  }
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + 'portfolio/index.html');
+});
+
+app.get(/^(.+)$/, function (req, res) {
+    res.sendFile(__dirname + req.params[0]);
 });
 
 var PORT = process.env.PORT || 3000;
