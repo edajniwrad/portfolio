80c8b50b0f61bb94cb7bd08033296a6bc120bd95
Darwin Donato <dvdonato-ctl@abs-cbn.com>
2020-03-25 20:28:48 +0800

    site update 3.1

--- src/app/lib/form/form.component.ts
+++ src/app/lib/form/form.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from './../../service/portfolio.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
+  public cvdetails = [];
+  private url: any = '/assets/data/profile.json';
 
-  constructor() { }
+  constructor(private service: PortfolioService) { }
 
   ngOnInit() {
+    this.service.get(this.url)
+    .subscribe(data => this.cvdetails = data);
   }
 
 }
--- src/app/lib/job/job.component.ts
+++ src/app/lib/job/job.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from './../../service/portfolio.service';
 
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html'
 })
 export class JobComponent implements OnInit {
+  public jobs = [];
+  private url: any = '/assets/data/jobs.json';
 
-  constructor() { }
+  constructor(private service: PortfolioService) { }
 
   ngOnInit() {
+    this.service.get(this.url)
+    .subscribe(data => this.jobs = data);
   }
 
 }
